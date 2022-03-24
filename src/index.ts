131fe63eaf6e49d9850ee1ff45752e97768c5e3b
Juan Alexander Carranza Alarcón <salmuzblues@gmail.com>
2022-03-23 23:04:31 -0500

    adding-cors

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { user } from './routes/user';
 import { json } from 'body-parser';
-
+const cors = require('cors');
 const app = express()
 app.use(json())
+app.use(cors())
 app.use(user)
 
 mongoose.connect('mongodb://localhost:27017/loop', () => {
   console.log('connected to database');
 })
-app.listen(3000, () => {
-  console.log('server is listening on port 3000');
+app.listen(5000, () => {
+  console.log('server is listening on port 5000');
 })
